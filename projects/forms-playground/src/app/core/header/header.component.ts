e0ece32a477da8d9373dfa03b71eda066fa05b81
Yama <yama.spec@yahoo.es>
2023-08-21 18:16:27 +1000

    Get a working code of form playground

--- projects/forms-playground/src/app/core/header/header.component.ts
+++ projects/forms-playground/src/app/core/header/header.component.ts
@@ -1,20 +1,21 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-header',
+    standalone: true,
+    imports: [CommonModule, RouterModule, FormsModule],
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
+    searchString: string = "";
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
 }
--- projects/forms-playground/src/app/playgrounds/template-forms/template-forms-page/template-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/template-forms/template-forms-page/template-forms-page.component.ts
@@ -1,28 +1,42 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserInfo } from '../../../core/user-info';
 
 @Component({
-  selector: 'app-template-forms-page',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './template-forms-page.component.html',
-  styleUrls: [
-    '../../common-page.scss',
-    '../../common-form.scss',
-    './template-forms-page.component.scss'
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-template-forms-page',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './template-forms-page.component.html',
+    styleUrls: [
+        '../../common-page.scss',
+        '../../common-form.scss',
+        './template-forms-page.component.scss'
+        ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TemplateFormsPageComponent implements OnInit {
 
-  constructor() { }
+    userInfo: UserInfo = {
+        firstName: '',
+        lastName: '',
+        nickName: '',
+        email: '',
+        yearOfBirth: 0,
+        passport: '',
+        fullAddress: '',
+        city: '',
+        postCode: 0
+    };
 
-  get years() {
-    const now = new Date().getUTCFullYear();
-    return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
-  }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    get years() {
+        const now = new Date().getUTCFullYear();
+        return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
+    }
+
+    ngOnInit(): void {
+    }
 
 }
