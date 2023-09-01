3eed36726e2de96912abda56ee6e9efe2288ad88
Yama <yama.spec@yahoo.es>
2023-09-01 10:30:25 +1000

    Strategies to reset the form

--- projects/forms-playground/src/app/playgrounds/template-forms/template-forms-page/template-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/template-forms/template-forms-page/template-forms-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { UserInfo } from '../../../core/user-info';
@@ -24,7 +24,7 @@ import { UniqueNicknameDirective } from '../validators/unique-nickname.directive
         ],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TemplateFormsPageComponent implements OnInit {
+export class TemplateFormsPageComponent implements OnInit, AfterViewInit {
 
     userInfo: UserInfo = {
         firstName: 'Marcus',
@@ -40,7 +40,10 @@ export class TemplateFormsPageComponent implements OnInit {
         confirmPassword: ''
     };
 
-    constructor() { }
+    // this.userInfo cannot be used because it may be different than the form's model structure.
+    // An ngForm may have groups, control names that differ from the UserInfo interface structure.
+    private initialFormValues: unknown;
+    @ViewChild(NgForm) formDirective!: NgForm;
 
     get isAdult() {
         return (new Date().getFullYear() - this.userInfo.yearOfBirth) >= 18;
@@ -51,25 +54,35 @@ export class TemplateFormsPageComponent implements OnInit {
         return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
     }
 
-    ngOnInit(): void {
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    ngAfterViewInit(): void {
+        // The task to add controls to my ngForm is a Promise.
+        // Thus, I have to put my control value assignment in the async tasks queue.
+        // Otherwise, it will execute first and will be overwritten by addControl() method.
+        queueMicrotask(() => {
+            this.initialFormValues = this.formDirective.value;
+        });
     }
 
-    onSubmitForm(form: NgForm, event: SubmitEvent) {
-        console.log("Form: ", form);
+    onSubmitForm(event: SubmitEvent) {
+        console.log("Form: ", this.formDirective.value);
         // Reset form values after submitting
-        this.userInfo = {
-            firstName: '',
-            lastName: '',
-            nickName: '',
-            email: '',
-            yearOfBirth: 0,
-            passport: '',
-            fullAddress: '',
-            city: '',
-            postCode: 0,
-            password: '',
-            confirmPassword: ''
-        };
+        // Strategy 1: Reset form values, validation statuses, making controls untouched, pristine, etc.
+        // form.resetForm();
+
+        // Strategy 2: Reset only control statuses but ot values
+        this.formDirective.resetForm(this.formDirective.value);
+        // Reset values to last submission values.
+        this.initialFormValues = this.formDirective.value;
+    }
+
+    onReset(event: Event) {
+        // To allow my method to execute and assign my values instead of the async Angular reset method.
+        event.preventDefault();
+        this.formDirective.resetForm(this.formDirective.value);
     }
 
 }
