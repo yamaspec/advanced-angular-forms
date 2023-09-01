c39b7a4ec47fb6bf267d95436d84f45c120621ed
Yama <yama.spec@yahoo.es>
2023-09-01 13:57:31 +1000

    FormGroup and FormArray

--- projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
@@ -1,28 +1,56 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-reactive-forms-page',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './reactive-forms-page.component.html',
-  styleUrls: [
-    '../../common-page.scss',
-    '../../common-form.scss',
-    './reactive-forms-page.component.scss'
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-reactive-forms-page',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule],
+    templateUrl: './reactive-forms-page.component.html',
+    styleUrls: [
+        '../../common-page.scss',
+        '../../common-form.scss',
+        './reactive-forms-page.component.scss'
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReactiveFormsPageComponent implements OnInit {
-  
-  get years() {
-    const now = new Date().getUTCFullYear();
-    return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
-  }
-  
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    form = new FormGroup({
+        firstName: new FormControl('Marcus'),
+        lastName: new FormControl('Latrell'),
+        nickName: new FormControl('Slinger'),
+        email: new FormControl('slinger@gmail.com'),
+        yearOfBirth: new FormControl(1984),
+        passport: new FormControl('PB123456'),
+        address: new FormGroup({
+            fullAddress: new FormControl(''),
+            city: new FormControl(''),
+            postCode: new FormControl(0)
+        }),
+        phones: new FormArray([
+            new FormControl('')
+        ])
+    });
+
+    get years() {
+        const now = new Date().getUTCFullYear();
+        return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
+    }
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    addPhone() {
+        let phone: string | undefined | null = this.form.controls.phones.controls[0]?.value;
+        if ((phone && phone.length > 0) || this.form.controls.phones.length === 0) {
+            this.form.controls.phones.insert(0, new FormControl(''));
+        }
+    }
+
+    removePhone(index: number) {
+        this.form.controls.phones.removeAt(index);
+    }
 
 }
