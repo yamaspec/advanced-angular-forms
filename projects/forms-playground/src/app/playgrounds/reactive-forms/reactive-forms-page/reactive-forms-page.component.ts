9210797c8793915e183f64a9d15cbb55248399b3
Yama <yama.spec@yahoo.es>
2023-09-04 17:15:27 +1000

    FormControl null, non-null, value by default

--- projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
@@ -17,28 +17,29 @@ import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular
 export class ReactiveFormsPageComponent implements OnInit {
 
     phoneLabels: string[] = ["Mobile", "Work", "Home"];
+    years = this.getYears();
     
     form = new FormGroup({
-        firstName: new FormControl('Marcus'),
-        lastName: new FormControl('Latrell'),
-        nickName: new FormControl('Slinger'),
+        firstName: new FormControl<string>('Marcus'),       // string and null values are allowed.
+        lastName: new FormControl('Latrell'),               // Could be Untyped: UntypedFormControl('Latrell')
+        nickName: new FormControl('Slinger', { nonNullable: true }),   // string only and define value by default.
         email: new FormControl('slinger@gmail.com'),
-        yearOfBirth: new FormControl(1984),
+        yearOfBirth: new FormControl(this.years[this.years.length - 1], { nonNullable: true }),  // number and value by default.
         passport: new FormControl('PB123456'),
-        address: new FormGroup({
-            fullAddress: new FormControl(''),
-            city: new FormControl(''),
-            postCode: new FormControl(0)
+        address: new FormGroup({                            // Could be Untyped: UntypedFormGroup({...})
+            fullAddress: new FormControl('', { nonNullable: true }),
+            city: new FormControl('', { nonNullable: true }),
+            postCode: new FormControl(0, { nonNullable: true })
         }),
-        phones: new FormArray([
+        phones: new FormArray([                             // Could be Untyped: UntypedFormArray([...])
             new FormGroup({
-                label: new FormControl(this.phoneLabels[0]),
+                label: new FormControl(this.phoneLabels[0], { nonNullable: true }),
                 phone: new FormControl('')
             })
         ])
     });
 
-    get years() {
+    private getYears() {
         const now = new Date().getUTCFullYear();
         return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
     }
@@ -51,7 +52,7 @@ export class ReactiveFormsPageComponent implements OnInit {
         let phone: string | undefined | null = this.form.controls.phones.controls[0]?.controls.phone.value;
         if ((phone && phone.length > 0) || this.form.controls.phones.length === 0) {
             this.form.controls.phones.insert(0, new FormGroup({
-                label: new FormControl(this.phoneLabels[0]),
+                label: new FormControl(this.phoneLabels[0], { nonNullable: true }),
                 phone: new FormControl('')
             }));
         }
