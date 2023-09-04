b14d6d82ef487a562639c423662aca01d0fb8019
Yama <yama.spec@yahoo.es>
2023-09-04 13:19:58 +1000

    Standalone Control in Reactive Forms

--- projects/forms-playground/src/app/core/header/header.component.ts
+++ projects/forms-playground/src/app/core/header/header.component.ts
@@ -1,21 +1,40 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [CommonModule, RouterModule, FormsModule],
+    imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
-    searchString: string = "";
+    // Template-Driven solution
+    // searchString: string = "";
+
+    // Reactive form solution
+    reactiveSearchString = new FormControl('');
 
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        // Reactive form solution
+        this.reactiveSearchString.valueChanges.pipe(
+            debounceTime(300),
+            distinctUntilChanged(),
+            // switchMapt(string => ...http.call(string...))
+        ).subscribe(console.log);
+    }
+
+    // Template driven solution
+    onSearchStringChange(event: string) {
+        // Not easy to use delay: setTimeout()
+        // Save previous value and compare with new one to know if it deserves to trigger the search.
+        console.log("onSearchStringChange: ", event);
+    }
 
 }
